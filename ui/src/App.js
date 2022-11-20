77f42476aed8634530730ae8b1b56ded9a0129ee
Kamil Skomro <kamilskomrokamil@gmail.com>
2022-11-20 04:18:42 +0100

    add selecting topic on start screen

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,36 @@
+import React, { useCallback } from "react";
+import Start from "./components/Start.js";
+import QuizComponent from "./components/QuizComponent.js";
+import blob from "./images/blob.png";
 
-import React, { useCallback } from 'react';
-import Start from './components/Start.js';
-import QuizComponent from './components/QuizComponent.js';
-import blob from './images/blob.png';
+export default function App() {
+  const [showStart, setShowStart] = React.useState(true);
+  const [topic, setTopic] = React.useState("");
 
-export default function App()
-{
-    
-    const [showStart, setShowStart] = React.useState(true)
-    
-    const wrappersetShowStart = useCallback(val => {
-        setShowStart(val);
-      }, [setShowStart]);
-    
-    function startQuiz()
-    {      
-        setShowStart(false)    
-    }
-    
-    
-    return(<div className='app'>
-                {showStart ? <Start startQuiz={startQuiz}/> : 
-                <QuizComponent showStartSetter={wrappersetShowStart}></QuizComponent> }
-                <img className='blob1' src={blob} alt=''/>
-                <img className='blob2' src={blob} alt=''/>
-            </div>)
+  const wrappersetShowStart = useCallback(
+    (val) => {
+      setShowStart(val);
+    },
+    [setShowStart]
+  );
+
+  function startQuiz(quizTopic) {
+    setTopic(quizTopic);
+    setShowStart(false);
+  }
+
+  return (
+    <div className="app">
+      {showStart ? (
+        <Start startQuiz={startQuiz} />
+      ) : (
+        <QuizComponent
+          showStartSetter={wrappersetShowStart}
+          topic={this.topic}
+        ></QuizComponent>
+      )}
+      <img className="blob1" src={blob} alt="" />
+      <img className="blob2" src={blob} alt="" />
+    </div>
+  );
 }
--- ui/src/components/Start.js
+++ ui/src/components/Start.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function Start(props)
-{
-    return(<div className='start-container'>
-        <h1 className='start-container-title'>Quizzical</h1>
-        <h2 className='start-container-subtitle'>Query Your Brain</h2>
-        <button className='start-container-button' onClick={props.startQuiz}>Start Quiz</button>
-    </div>)
+export default function Start(props) {
+  const [topic, setTopic] = React.useState("");
+
+  function handleChange({ target }) {
+    setTopic(target.value);
+  }
+
+  return (
+    <div className="start-container">
+      <h1 className="start-container-title">Quizzical</h1>
+      <h2 className="start-container-subtitle">Query Your Brain</h2>
+      <input
+        type="text"
+        name="topicBox"
+        placeholder="Enter topic here..."
+        value={topic}
+        onChange={handleChange}
+      />
+      <button
+        className="start-container-button"
+        onClick={props.startQuiz(topic)}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
 }
