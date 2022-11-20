ae99b9463030815048715b01bd83752ca1ef51f1
NoOne <micwar@interia.pl>
2022-11-20 02:59:52 +0100

    move logic from App.js to QuizComponent.js

--- ui/src/components/QuestHeader.js
+++ ui/src/components/QuestHeader.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import Tooltip from '@mui/material/Tooltip';
+import Grid from '@mui/material/Grid';
 
 export default function QuestHeader(props)
 {
         
-    return(<div className='quiz-container-question-header'>
-        <h1 className='quiz-container-question-title' dangerouslySetInnerHTML={{__html: props.question}}/>
-        </div>
+    return(<Grid container spacing={2}>
+            <Grid item xs={7}>
+                <h1 className='quiz-container-question-title' dangerouslySetInnerHTML={{__html: props.question}}/>
+            </Grid>
+            <Grid item xs={0.5}>
+                <Tooltip title="Regenerate">
+                    <IconButton>
+                        <RefreshIcon />
+                    </IconButton>
+                </Tooltip>
+            </Grid>
+            <Grid item xs={0.5}>
+                <Tooltip title="Delete" color="error">
+                    <IconButton>
+                        <DeleteIcon />
+                    </IconButton>
+                </Tooltip>
+            </Grid>
+        </Grid>
     )
 }
---
+++ ui/src/components/QuizComponent.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import Quest from './Quest.js';
+
+export default function QuizComponent(props)
+{
+    const [score, setScore] = React.useState(0)
+    const [showAnswers, setShowAnswers] = React.useState(false)
+    const [questions, setQuestions] = React.useState([])
+    const [allComplete, setAllComplete] = React.useState(false)
+    
+    
+    function playAgain()
+    {
+        props.showStartSetter(true)
+        setShowAnswers(false)
+        setAllComplete(false)
+    }
+    
+    function checkAnswers()
+    {
+        setShowAnswers(true)
+    }
+    
+    function selectAnswer(event,quest_id,option_id)
+    {
+        setQuestions(function(prev) {
+            return(questions.map(function(quest,qid) {
+                if(quest_id===qid){
+                    return({...quest, selected_answer:option_id})
+                }else{
+                    return(quest)
+                }
+                
+            }))
+        })
+    }
+    
+    React.useEffect(() => {
+        var count = 0;
+        for(var i = 0; i < questions.length; i++)
+        {
+          if (typeof questions[i].selected_answer !== 'undefined')
+          {
+            if(questions[i].options[questions[i].selected_answer] === questions[i].correct_answer)
+            {
+              count++;
+            }
+          }
+        }
+        setScore(count)
+    },[showAnswers])
+    
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5")
+            .then(res => res.json())
+            .then(data => setQuestions(data.results.map(function(question) {
+                return({question:question.question,
+                        options:question.incorrect_answers.concat([question.correct_answer]).map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value),
+                        selected_answer:undefined,
+                        correct_answer:question.correct_answer})
+            })))
+            }
+    ,[])
+    
+    React.useEffect(() => { 
+        setAllComplete(questions.every((quest) => typeof quest.selected_answer !== 'undefined'))
+    }, [questions])
+    
+    const quests = questions.map(function(question,index) {
+        return(<Quest
+                    key={index}
+                    question={question}
+                    showAnswers={showAnswers}
+                    selectAnswer={selectAnswer}
+                    id={index}
+                />)
+    })
+        
+    return(<div className='quiz-container'>
+                {quests}
+                {showAnswers ? 
+                    <div className='button-container'>
+                        <h3 className='button-container-score'>{"You scored " + score + "/5 correct answers"}</h3>
+                        <button className='button' onClick={playAgain}>Play Again</button>
+                    </div> 
+                    :
+                    <button className='button' disabled={!allComplete} onClick={checkAnswers}>Check Answers</button>}
+            </div>)
+}
